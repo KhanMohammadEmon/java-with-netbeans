40200c4926312f32c4e9f858e77e86c7f6ab0ed1
unknown <emon122734@gmail.com>
2021-08-06 00:19:57 +0600

    Encapulation add here

---
+++ Lecture4/src/EncapTest.java
@@ -0,0 +1,7 @@
+public class EncapTest {
+    public static void main(String[] args) {
+       Encapsulation enca = new Encapsulation();
+       enca.setNumber(45);
+        System.out.println(enca.getNumber());
+    }
+}
---
+++ Lecture4/src/Encapsulation.java
@@ -0,0 +1,11 @@
+public class Encapsulation {
+    private int number;
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
