4a72daec2890d3a17669d0be5f7f163faa4363ae
Emon Khan <62669422+KhanMohammadEmon@users.noreply.github.com>
2021-08-12 11:34:14 +0600

    Update AnimalHeredityTest.java

--- Practice/src/SP2018/A/AnimalHeredityTest.java
+++ Practice/src/SP2018/A/AnimalHeredityTest.java
@@ -2,23 +2,7 @@ package SP2018.A;
 
 public class AnimalHeredityTest {
     public static void main(String[] args) {
-        Animal a = new Animal("Ethon","011",23);
-        System.out.println(a.getName());
-        System.out.println(a.getId());
-        System.out.println(a.getCp());
-
-        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-
-        a.setName("EK");
-        a.setId("022");
-        a.setCp(34);
-
-        System.out.println(">>>>>>>>After Result SetOut>>>>>>>");
-
-        System.out.println(a.getName());
-        System.out.println(a.getId());
-        System.out.println(a.getCp());
-
+        
 
     }
 }
