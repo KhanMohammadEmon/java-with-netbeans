eca8549c4ff8f91a34fc8b2862bd1a8d9dbbf0a4
KhanMohammadEmon <sagor.ms766@gmail.com>
2021-08-31 12:51:34 +0600

    add 2

--- Extra_exam/exam/src/Driver1.java
+++ Extra_exam/exam/src/Driver1.java
@@ -1,12 +1,12 @@
 public class Driver1 {
     public static void main(String[] args) {
      Shape s1 = new Rectangle();
-     s1.setDim1(4);
-     s1.setDim2(8);
+     s1.dim1=4;
+     s1.dim2=8;
      s1.printArea();
 
         Shape s2 = new Circle();
-        s2.setDim2(5);
+        s2.dim2=5;
         s2.printArea();
     }
 }
---
+++ Extra_exam/exam/src/Vector2D.java
@@ -0,0 +1,30 @@
+public class Vector2D{
+    double x, y;
+    public Vector2D(){
+        x=0;
+        y=0;
+    }
+   public Vector2D(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+   public double Multiplied(double u,double v)
+   {
+       double multi = (u*x)*(v*x)+(u*y)*(v*y);
+      return multi;
+   }
+   public double Multiplied(double u)
+   {
+       double multi = (u*x)+(u*y);
+      return multi;
+   }
+
+    public static void main(String[] args) {
+        Vector2D v1 = new Vector2D(1,2);
+
+
+        System.out.println( v1.Multiplied(2));
+        System.out.println(v1.Multiplied(2,3));
+    }
+
+}
