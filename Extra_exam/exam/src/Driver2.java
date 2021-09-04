5580c8091d802fd2a3d1313b5a06aa12477c3c8f
KhanMohammadEmon <sagor.ms766@gmail.com>
2021-09-04 15:09:08 +0600

    No 2

---
+++ Extra_exam/exam/src/Driver2.java
@@ -0,0 +1,8 @@
+public class Driver2 {
+    public static void main(String[] args) {
+        Employees e1 = new Employees();
+        e1.setSalary(10000);
+        e1.tax();
+  
+    }
+}
--- Extra_exam/exam/src/Employees.java
+++ Extra_exam/exam/src/Employees.java
@@ -1,4 +1,18 @@
 public class Employees {
     public String name;
-    
+    public int age;
+    private double salary=0;
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+    public  void tax()
+    {
+        double s_tax = (getSalary()*5)/100;
+        System.out.println("His/her total tax is : "+s_tax);
+    }
 }
